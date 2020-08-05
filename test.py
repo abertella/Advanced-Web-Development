81fc47716e2322a6f193da90dbb26da4f1a3195c
andrew_bertella <berba@uab.edu>
2020-08-05 18:20:38 -0500

    New Database

--- test.py
+++ test.py
@@ -7,5 +7,7 @@ client = pymongo.MongoClient('mongodb://localhost:27017/')
 db = client['mydatabase']
 coll = db['events']
 
+db.coll.delete_many({'id': '1'})
+
 for x in coll.find():
     pprint.pprint(x)
