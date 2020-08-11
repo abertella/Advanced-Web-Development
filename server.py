b120c330af936f44ac0ced5c2bff0d7c21d75e16
andrew_bertella <berba@uab.edu>
2020-08-11 10:50:19 -0500

    Modified server.py

--- server.py
+++ server.py
@@ -11,13 +11,16 @@ db = client['mydatabase']
 coll = db['events']
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected')
-        while True:
-            data = conn.recv(1024)
-            print(data)
-            id = coll.insert_one(json.loads(data))
-            print(id)
+    try:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print('Connected')
+            while True:
+                data = conn.recv(1024)
+                print(data)
+                id = coll.insert_one(json.loads(data))
+                print(id)
+    finally:
+        s.close()
